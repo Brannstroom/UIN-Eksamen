02804153507da396c1decfdfc566e8f7ca8ce673
paxtrixx <vadimkiisel@gmail.com>
2021-05-09 12:34:41 +0200

    main
    awd

--- project/client/src/utils/client.js
+++ project/client/src/utils/client.js
@@ -3,7 +3,8 @@ import sanityClient from '@sanity/client';
 const options = {
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: process.env.REACT_APP_SANITY_DATASET_NAME,
-  token: process.env.REACT_APP_SANITY_WRITE_TOKEN
+  token: process.env.REACT_APP_SANITY_WRITE_TOKEN,
+  tame: process.env.REACT_APP_ADD_YOUR_NAME,
 };
 
 const client = sanityClient({
