0a3a1e1fc3efa0c36d8f72f3038b7fc8d57b87dc
Peter Brännström <54285305+Brannstroom@users.noreply.github.com>
2021-05-11 23:32:46 +0200

    Some work

--- project/client/src/components/Posts.js
+++ project/client/src/components/Posts.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import sanityClient from '../client.js';
 import Post from './Post';
+import '../styles/nyheter.scss';
 
-export default function Posts() {
+export default function Posts({ cl }) {
   const [allPostsData, setAllPosts] = useState(null);
-  const [postAmount, setPostAmount] = useState(3);
+  const [postAmount, setPostAmount] = useState(12);
 
   const loadMore = () => {
     setPostAmount((previousValue) => previousValue + 6);
@@ -18,31 +19,38 @@ export default function Posts() {
         slug,
         'author': author->name,
         imageAlt,
-        'categories': categories->name,
+        'category': category->title,
+        body,
         mainImage{
           asset->{
           _id,
           url
-        },
-      }
-    }`
+          },
+        }
+      }`
       )
       .then((data) => setAllPosts(data))
       .catch(console.error);
   }, []);
 
+  function handleBody(body) {
+    console.log(body);
+    return body;
+  }
+
   return (
-    <div className="posts">
+    <div className={cl}>
       {allPostsData &&
         allPostsData
           .slice(0, postAmount)
           .map((post) => (
             <Post
+              category={post.category}
               title={post.title}
               image={post.mainImage.asset.url}
               alt={post.imageAlt}
               link={post.slug.current}
-              categories={post.categories}
+              body={handleBody(post.body)}
             />
           ))}
       <div className="loadMoreButton">
--- project/client/src/pages/Hjem.js
+++ project/client/src/pages/Hjem.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Posts from '../components/Posts';
-import ViewSwitch from '../components/ViewSwitch';
 import '../styles/nyheter.scss';
 
 export default function Hjem() {
+  const [view, setView] = useState('gridView');
+
+  function clickHandler() {
+    if (view === 'gridView') {
+      setView('listView');
+    } else {
+      setView('gridView');
+    }
+    console.log(view);
+  }
+
   return (
     <>
-      <ViewSwitch />
-      <Posts />
+      <div className="mid">
+        <label className="rocker rocker-small">
+          <input type="checkbox" onClick={clickHandler} />
+          <span className="switch-left"> Liste </span>
+          <span className="switch-right"> Grid </span>
+        </label>
+      </div>
+      <Posts cl={view} />
     </>
   );
 }
