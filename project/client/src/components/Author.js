bb5dce96e23ea2ff2510ca1e5b120a9047cbf460
paxtrixx <vadimkiisel@gmail.com>
2021-05-11 17:47:33 +0200

    små ting
    trenger en pause så pusher siste ting før jeg tar en

--- project/client/src/components/Author.js
+++ project/client/src/components/Author.js
@@ -1,6 +1,10 @@
 const Author = ({ name, image, alt, rolle, bio }) => (
   <div className="authordiv">
-    <img style={{width: 150, height: 150, borderRadius: 400/ 2}} src={image} alt={alt} />
+    <img
+      style={{ width: 150, height: 150, borderRadius: 400 / 2 }}
+      src={image}
+      alt={alt}
+    />
     <div className="authorbiocss">
       <h3>{name}</h3>
       <p>{rolle}</p>
